44dbb93ca07b7a6f9a410e374dd9c0c4c4082fd1
h982513 <h982513@stud.u-szeged.hu>
2024-10-23 09:36:30 +0200

    frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Üdvözlünk a dobókocka projekt oldalán</h1>
+<h2>Készítették</h2>
+<ol>
+    <li>Kis-Horváth Katalin Anna</li>
+    <li>Varga Klaudia</li>
+    <li>Varga Dominik</li>
+    <li>Dinnyés Dávid</li>
+</ol>
