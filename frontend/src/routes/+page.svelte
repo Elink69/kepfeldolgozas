be1d46d97d421f390e5555e7c35f8fc7504d0d59
h982513 <h982513@stud.u-szeged.hu>
2024-10-23 11:37:47 +0200

    frontend módosítás

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,19 @@
-<h1>Üdvözlünk a dobókocka projekt oldalán</h1>
-<h2>Készítették</h2>
-<ol>
-    <li>Kis-Horváth Katalin Anna</li>
-    <li>Varga Klaudia</li>
-    <li>Varga Dominik</li>
-    <li>Dinnyés Dávid</li>
-</ol>
-<p><a href="/dice">Itt</a> lehet a kockákat leszámolni</p>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<style>
-    
-</style>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Dobókocka projekt</h1>
+		<p>A projekt a digitális képfeldolgozás beadandó miatt készült</p>
+		<h2>Készítették:</h2>
+		<ul>
+			<li>Kis-Horváth Katalin Anna</li>
+			<li>Varga Klaudia</li>
+			<li>Varga Dominik</li>
+			<li>Dinnyés Dávid</li>
+		</ul>
+		<a href="/dice" class="btn variant-filled">
+			<span>(icon)</span>
+			<span>Dobókocka számíó </span>
+		</a>
+	</div>
+</div>
--- frontend/src/routes/dice/+page.svelte
+++ frontend/src/routes/dice/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Kockák</h1>
-<a href="/">Vissza a főlapra</a>
+<div class='container h-full mx-auto flex justify-center items-center'>
+    <h1 class='h1'>Kockák</h1>
+    <p></p>
+</div>
