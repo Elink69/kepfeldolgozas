4dadeaf415eebf5e07a840f9285fe4b65f27f6e3
TricksterCode210 <david.dinnyes98@gmail.com>
2024-11-09 15:44:58 +0100

    Frontend + backend összekapcsolás

--- frontend-react/src/components/DicesPage.js
+++ frontend-react/src/components/DicesPage.js
@@ -1,8 +1,11 @@
 import {useEffect, useState} from 'react'
 import {Button} from 'primereact/button'
+import axios from 'axios';
 
 const DicePages = () => {
     const [rawImage, setRawImage] = useState()
+    const [fileUpload, setFileUpload] = useState()
+    const [response, setResponse] = useState()
 
     const handleImageChange = (e) => {
         const file = e.target.files[0]
@@ -10,11 +13,31 @@ const DicePages = () => {
             return
         }
         const reader = new FileReader()
+        setFileUpload(file)
 		reader.onloadend = () => {
 			const imageDataUri = reader.result
 			setRawImage(imageDataUri)
 		}
 		reader.readAsDataURL(file)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append("file", fileUpload);
+
+        try {
+            // Replace with your actual API endpoint
+            const response = await axios.post('http://127.0.0.1:8000/upload_image', formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data',  // Ensure the proper content type for file uploads
+              },
+            });
+
+            setResponse(response.data);  // Set the response from the API
+          } catch (err) {
+            console.error("Error message: ", err);
+          }
     }
 
     const deleteResult = () => {
@@ -36,6 +59,7 @@ const DicePages = () => {
                     accept={"image/*"}
                 />
                 <Button label={'Törlés'} severity={'danger'} hidden={!rawImage} className={'me-3'} onClick={e => deleteResult()}/>
+                <Button label={'Beküldés'} severity={'info'} hidden={!rawImage} className={'me-3'} onClick={handleSubmit}/>
                 <img src={rawImage} hidden={!rawImage} height={'250px'} width={'250px'} alt="kép"/>
             </div>
         </>
